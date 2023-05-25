0ef0649f3bb56e0cc82fd15b32c400b2a1333964
Humesh Deshmukh <112746090+humeshdeshmukh@users.noreply.github.com>
2023-05-25 13:05:41 +0530

    new code

--- mydirectory/prog-doubly-linklist02.c
+++ mydirectory/prog-doubly-linklist02.c
@@ -1,102 +1,130 @@
 #include<stdio.h>
-#include<malloc.h>
+#include<stdlib.h>
+
 struct dnode
 {
     struct dnode *llink;
     int data;
     struct dnode *rlink;
-}*start,*p,*q;
+};
+
+struct dnode *start, *p, *q;
+
 void insert_dll();
 void delete_dll();
 void display_dll();
+
 int main()
 {
     int ch;
-    start=NULL;
+    start = NULL;
+
     while(1)
     {
-        printf("\n 1. Insert");
-        printf("\n 2. Delete");
-        printf("\n 3. Display");
-        printf("\n 4. Exit");
-        printf("\n Enter your choice: ");
-        scanf("%d",&ch);
+        printf("\n1. Insert");
+        printf("\n2. Delete");
+        printf("\n3. Display");
+        printf("\n4. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+
         switch(ch)
         {
             case 1: insert_dll();
-            break;
+                    break;
             case 2: delete_dll();
-            break;
+                    break;
             case 3: display_dll();
-            break;
+                    break;
             case 4: exit(0);
-            break;
-            default: printf("\n Invalid choice");
+                    break;
+            default: printf("\nInvalid choice");
         }
     }
 }
+
 void insert_dll()
 {
-    p=(struct dnode*)malloc(sizeof(struct dnode));
-    printf("\n Enter data: ");
-    scanf("%d",&p->data);
-    if(start==NULL)
+    p = (struct dnode*)malloc(sizeof(struct dnode));
+    printf("\nEnter data: ");
+    scanf("%d", &p->data);
+    p->llink = NULL;
+    p->rlink = NULL;
+
+    if(start == NULL)
     {
-        p->rlink=NULL;
-        p->llink=NULL;
-        start=p;
+        start = p;
     }
     else
     {
-        p->llink=NULL;
-        p->rlink=start;
-        p->rlink->llink=p;
-        start=p;
+        p->rlink = start;
+        start->llink = p;
+        start = p;
     }
+
+    printf("Data inserted successfully!\n");
 }
+
 void delete_dll()
 {
-    if(start==NULL)
+    if(start == NULL)
     {
-        printf("\n Empty list");
+        printf("\nEmpty list");
     }
     else
     {
-        p=start;
-        p->rlink->llink=NULL;
-        start=p->rlink;
-        p->rlink=NULL;
+        p = start;
+        start = start->rlink;
+
+        if(start != NULL)
+        {
+            start->llink = NULL;
+        }
+
         free(p);
+        printf("\nNode deleted successfully!");
     }
 }
+
 void display_dll()
 {
     int choice;
-    printf("\n Traversal path");
-    printf("\n 1. Forward direction");
-    printf("\n 2. Backward direction");
-    printf("\n Please select: ");
-    scanf("%d",&choice);
+
+    printf("\nTraversal path");
+    printf("\n1. Forward direction");
+    printf("\n2. Backward direction");
+    printf("\nPlease select: ");
+    scanf("%d", &choice);
+
     switch(choice)
     {
-        case 1: p=start;
-            while(p!=NULL)
-            {
-            printf("\t %d",p->data);
-            p=p->rlink;
-            }
-        break;
-        case 2: p=start;
-            while(p->rlink!=NULL)
-            {
-                p=p->rlink;
-            }
-            while(p!=NULL)
-            {
-                printf("\t %d",p->data);
-                p=p->llink;
-            }
-            break;
-            default: printf("\n Invalid choice");
+        case 1: p = start;
+                printf("\nForward direction: ");
+                while(p != NULL)
+                {
+                    printf("\t%d", p->data);
+                    p = p->rlink;
+                }
+                break;
+
+        case 2: p = start;
+                if(p == NULL)
+                {
+                    printf("\nEmpty list");
+                    break;
+                }
+                while(p->rlink != NULL)
+                {
+                    p = p->rlink;
+                }
+                printf("\nBackward direction: ");
+                while(p != NULL)
+                {
+                    printf("\t%d", p->data);
+                    p = p->llink;
+                }
+                break;
+
+        default: printf("\nInvalid choice");
     }
 }
